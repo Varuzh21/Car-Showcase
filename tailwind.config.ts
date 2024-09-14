2a7b6cddcd5b2f2e358c90217d0e62e28505baff
Varuzh21 <varuzhan.grigoryan@urartian.am>
2024-09-14 20:22:33 +0400

    add Hero component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,40 @@
-import type { Config } from "tailwindcss";
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  mode: 'jit',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'black-100': '#2B2C35',
+        'primary-blue': {
+          DEFAULT: '#2B59FF',
+          100: '#F5F8FF',
+        },
+        'secondary-orange': '#f79761',
+        'light-white': {
+          DEFAULT: 'rgba(59,60,152,0.03)',
+          100: 'rgba(59,60,152,0.02)',
+        },
+        grey: '#747A88',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      backgroundImage: {
+        pattern: "url('/pattern.png')",
+        'hero-bg': "url('/hero-bg.png')",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
