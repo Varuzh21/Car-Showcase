eef6b43e77270fdbfd305ba2dbfd2445a0267332
varuzh21 <varuzhan.grigoryan@urartian.am>
2024-09-19 00:08:31 +0400

    change className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home({searchParams}: any ) {
         <div className="home__filters">
           <SearchBar />
 
-          <div className="home__filter-container ">
+          <div className="home__filters-container">
             <CustomFilter title="fuel" options={fuels}/>
             <CustomFilter title="year" options={yearsOfProduction} />
           </div>
